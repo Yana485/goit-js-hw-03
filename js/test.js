c65b7053b0ad27c48c10278eb346bf1ef52800a4
Yana485 <105270905+Yana485@users.noreply.github.com>
2024-01-30 21:36:50 +0000

    test1.1

--- js/test.js
+++ js/test.js
@@ -1,9 +1,15 @@
-function calculateTotalPrice(order){
-let a = 0;
-  for (let i = 0; i <= order.length; i += 1){
-    a += order[i];
-    }
-    return a;
+function getCommonElements(array1, array2) {
+  const newArray = [];
+  for (let i = 0; i < array1.length; i += 1){
+    if(array2.includes(array1[i])){
+      newArray.push(array1[i]);
+    }    
+  }
+    return newArray;
 }
 
-console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(getCommonElements([1, 2, 3], [2, 4]));
+console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
